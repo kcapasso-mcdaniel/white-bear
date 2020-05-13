354af7b4d1461779d6d553a7ab63cf104ccfc982
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-05-12 21:47:04 -0700

    add card

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#save-imagery").click(function () {
+   $("#overlay-success").toggleClass("d-none d-flex");
+});
+$("#back-to-answer-imagery").click(function () {
+   $("#overlay-danger").toggleClass("d-none d-flex");
+});
+$("#next-answer").click(function () {
+   $("#overlay-success").toggleClass("d-none d-flex");
+});
