ab916b2ff0315cc019f6689eae148a2c64695e1d
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-05-14 18:47:59 -0700

    added functions

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+$("#sign-up-button").click(function () {
+   $("#sign-up-form").toggleClass("d-none");
+   $("#sign-up-button").remove();
+});
 $("#save-imagery").click(function () {
    $("#overlay-success").toggleClass("d-none d-flex");
 });
-$("#sign-up-button").click(function () {
-   $("#sign-up-form").toggleClass("d-none d-flex");
+$("#back-to-answer-imagery").click(function () {
+   $("#overlay-danger").toggleClass("d-none d-flex");
+});
+$("#check-show-delete-button").click(function () {
+   $("#delete-button").toggleClass("d-none");
+});
+$(".dropdown").click(function () {
+   $(".dropdown-toggle").toggleClass("show");
 });
