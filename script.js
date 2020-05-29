670c629f7208910bedd6b7fe9e8ba581f87366c6
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-05-29 10:21:10 -0700

    add common passoword error

--- script.js
+++ script.js
@@ -4,6 +4,422 @@ $("#sign-up-button").click(function () {
    $("#sign-up-button").remove();
 });
 
+// Create the date function
+// This function generates the current date and time in two-digit formatting
+var getToday = "";
+var timeMilliseconds = "";
+
+function createTheDate() {
+   var getTodayDate = new Date();
+   var dateYear = getTodayDate.getFullYear();
+   console.log(dateYear);
+   dateYear = dateYear.toString().substr(-2);
+   var dateMonth = getTodayDate.getMonth() + 1;
+   console.log(dateMonth);
+   var dateDay = getTodayDate.getDate();
+   console.log(dateDay);
+   var timeHours = getTodayDate.getHours();
+   console.log(timeHours);
+   var timeMinutes = getTodayDate.getMinutes();
+   console.log(timeMinutes);
+   var timeSeconds = getTodayDate.getSeconds();
+   console.log(timeSeconds);
+   timeMilliseconds = getTodayDate.getMilliseconds();
+   console.log(timeMilliseconds);
+
+   getToday =
+      "" + //turns it into a readable string
+      dateYear +
+      addTwoPadding(dateMonth) +
+      addTwoPadding(dateDay) +
+      addTwoPadding(timeHours) +
+      addTwoPadding(timeMinutes) +
+      addTwoPadding(timeSeconds);
+   console.log(getToday);
+}
+
+// Padding functions for date and time to return proper number of digits
+
+function addThreePadding(num) {
+   // A1: number
+   // R: string
+   var numAsString = String(num);
+   if (numAsString.length === 1) {
+      return "00" + numAsString; // 4 returns "004"
+   } else if (numAsString.length === 2) {
+      return "0" + numAsString; // 44 returns "044"
+   } else {
+      return numAsString;
+   }
+}
+function addTwoPadding(num) {
+   // A1: number
+   // R: string
+   var numAsString = String(num);
+   if (numAsString.length === 1) {
+      return "0" + numAsString; // 4 returns "04"
+   } else {
+      return numAsString;
+   }
+}
+
+// Variable for PassWord Validation
+var passwordList = [
+   "x002tp00",
+   "x4ww5qdr",
+   "x72jhhu3z",
+   "xaccess2",
+   "xakep1234",
+   "xboxlive",
+   "xcalibur",
+   "xcountry",
+   "xenocide",
+   "xenogear",
+   "xenophobia",
+   "xenophobic",
+   "xenophon",
+   "xexeylhf",
+   "xianzong",
+   "xiaoping",
+   "xiaoyua123",
+   "xinjiang",
+   "xohzi3g4",
+   "xpressmusic",
+   "xsvnd4b2",
+   "xsw21qaz",
+   "xsw23edc",
+   "xthtgfirf",
+   "xtutdfhf",
+   "xuanzang",
+   "xuanzong",
+   "xufrgemw",
+   "xxx12345",
+   "xxxp455w0rd5",
+   "xxxxxxx1",
+   "xyh28af4",
+   "xylophone",
+   "xzsawq21",
+   "y'knowwhati'msayin",
+   "y'understand",
+   "yachting",
+   "yaglasph",
+   "yakovlev",
+   "yakushova",
+   "yamagata",
+   "yamaha12",
+   "yamahar1",
+   "yamahar6",
+   "yamakasi",
+   "yamamoto",
+   "yamanashi",
+   "yamashita",
+   "yammering",
+   "yangzhou",
+   "yankeemp",
+   "yankees0",
+   "yankees1",
+   "yankees2",
+   "yankees3",
+   "yankees4",
+   "yankees7",
+   "yankees9",
+   "yankovic",
+   "yanochka",
+   "yanshi1982",
+   "yanukovych",
+   "yarborough",
+   "yarbrough",
+   "yardbird",
+   "yardbirds",
+   "yardstick",
+   "yarmouth",
+   "yaroslav",
+   "yaroslavl",
+   "yasacrac",
+   "yasuhiro",
+   "ybrbnbyf",
+   "ybrbnf_25",
+   "ybrbnjcbr",
+   "ybrjkftd",
+   "ybrjkftdbx",
+   "ybrjkftdyf",
+   "ycwvrxxh",
+   "yeahbaby",
+   "yeahrigh",
+   "yeahright",
+   "year2000",
+   "year2005",
+   "yearbook",
+   "yearbooks",
+   "yearning",
+   "yearnings",
+   "yekaterinburg",
+   "yelburton",
+   "yelena03",
+   "yellow12",
+   "yellow22",
+   "yellowhead",
+   "yellowish",
+   "yellowknife",
+   "yellowstone",
+   "yelverton",
+   "yemenite",
+   "yengeese",
+   "yeomanry",
+   "yes90125",
+   "yeshivas",
+   "yessenia",
+   "yesterda",
+   "yesterday",
+   "yesterdays",
+   "yfcnfcmz",
+   "yfcntymrf",
+   "yfcnz123",
+   "yfcnzvjz",
+   "yfcnzyfcnz",
+   "yfdbufnjh",
+   "yfeiybrb",
+   "yfgjktjy",
+   "yfhrjnbrb",
+   "yfltymrf",
+   "yfnfitymrf",
+   "yfxfkmybr",
+   "ygfxbkgt",
+   "yggdrasi",
+   "yielding",
+   "yildirim",
+   "yingling",
+   "yingyang",
+   "yitzchak",
+   "yjdbrjdf",
+   "yjdjcnbf",
+   "yjdsqgfhjkm",
+   "yjdsqujl",
+   "yjdujhjl",
+   "yjhbkmcr",
+   "ym3cautj",
+   "yogibear",
+   "yogyakarta",
+   "yohannan",
+   "yokohama",
+   "yokosuka",
+   "yokozuna",
+   "yorkshir",
+   "yorkshire",
+   "yorktown",
+   "yorkville",
+   "yosemite",
+   "yoshihiro",
+   "youandme",
+   "youlanda",
+   "youngblood",
+   "youngest",
+   "youngman",
+   "youngone",
+   "youngster",
+   "youngsters",
+   "youngstown",
+   "yourmama",
+   "yourmom1",
+   "yourname",
+   "yourself",
+   "yourselves",
+   "yousuck1",
+   "youthful",
+   "yoyodyne",
+   "ypsilanti",
+   "yqlgr667",
+   "yqmbevgk",
+   "yr8wdxcq",
+   "ytdxz2ca",
+   "ytngfhjkz",
+   "ytnhjufnm",
+   "ytrhjvfyn",
+   "ytyfdbcnm",
+   "yudhoyono",
+   "yugoslav",
+   "yugoslavia",
+   "yugoslavian",
+   "yuitre12",
+   "yujyd360",
+   "yuletide",
+   "yushchenko",
+   "yvelines",
+   "yvtte545",
+   "yxkck878",
+   "yy5rbfsc",
+   "yyyyyyy1",
+   "yzerman1",
+   "z1234567",
+   "z123456789",
+   "z123456z",
+   "z1x2c3v4",
+   "z1x2c3v4b5",
+   "z1x2c3v4b5n6m7",
+   "z1z2z3z4",
+   "z3cn2erv",
+   "zacatecas",
+   "zacharia",
+   "zachariah",
+   "zacharias",
+   "zachary1",
+   "zaharova",
+   "zakamatak",
+   "zalewski",
+   "zalgiris",
+   "zamarripa",
+   "zambales",
+   "zamboanga",
+   "zambrano",
+   "zamindar",
+   "zamoyski",
+   "zanesville",
+   "zantopia",
+   "zanzibar",
+   "zapatista",
+   "zaphod42",
+   "zaporizhia",
+   "zapruder",
+   "zaq!2wsx",
+   "zaq11qaz",
+   "zaq12345",
+   "zaq123wsx",
+   "zaq12qaz",
+   "zaq12wsx",
+   "zaq1xsw2",
+   "zaq1xsw2cde3",
+   "zaqwsx123",
+   "zaqwsxcde",
+   "zaqwsxcderfv",
+   "zaqxsw123",
+   "zaqxswcde",
+   "zaragoza",
+   "zaratustra",
+   "zarzuela",
+   "zasranec",
+   "zatunica",
+   "zawahiri",
+   "zaxscdvf",
+   "zbigniew",
+   "zcfvfzkexifz",
+   "zcxfcnkbdf",
+   "zcxfcnkbdfz",
+   "zealander",
+   "zealanders",
+   "zebra123",
+   "zechariah",
+   "zeebrugge",
+   "zeitgeist",
+   "zeitschrift",
+   "zeljeznicar",
+   "zemanova",
+   "zendejas",
+   "zenit2011",
+   "zeppelin",
+   "zeringue",
+   "zerocool",
+   "zesyrmvu",
+   "zhaozong",
+   "zhejiang",
+   "zhengzhou",
+   "zhjckfdf",
+   "zhongguo",
+   "zhongshan",
+   "zhongshu",
+   "zhongzong",
+   "zhytomyr",
+   "zidane10",
+   "ziegfeld",
+   "zielinski",
+   "zigazaga",
+   "ziggy123",
+   "zildjian",
+   "zillions",
+   "zimbabwe",
+   "zimbabwean",
+   "zimmerma",
+   "zimmerman",
+   "zimmermann",
+   "zinedine",
+   "zinfandel",
+   "zinoviev",
+   "zionists",
+   "zipdrive",
+   "zippy123",
+   "zirconia",
+   "zirconium",
+   "zjses9evpa",
+   "zldej102",
+   "zoidberg",
+   "zolushka",
+   "zombie13",
+   "zookeeper",
+   "zoological",
+   "zoologist",
+   "zooplankton",
+   "zootsuit",
+   "zoroaster",
+   "zoroastrian",
+   "zoroastrianism",
+   "zoroastrians",
+   "zorro123",
+   "zpflhjn1",
+   "zqjphsyf6ctifgu",
+   "zrjdktdf",
+   "zse45rdx",
+   "zse4xdr5",
+   "zsecyus56",
+   "zucchero",
+   "zucchini",
+   "zuckerberg",
+   "zuckerman",
+   "zukowski",
+   "zulfikar",
+   "zulfiqar",
+   "zvonareva",
+   "zweibrucken",
+   "zx123456",
+   "zx123456789",
+   "zxasqw12",
+   "zxc12345",
+   "zxc123456",
+   "zxc123zxc",
+   "zxcasd123",
+   "zxcasdqw",
+   "zxcasdqwe",
+   "zxcasdqwe123",
+   "zxcqweasd",
+   "zxcv1234",
+   "zxcv4321",
+   "zxcvasdf",
+   "zxcvb123",
+   "zxcvb1234",
+   "zxcvb12345",
+   "zxcvbasdfg",
+   "zxcvbn12",
+   "zxcvbn123",
+   "zxcvbn123456",
+   "zxcvbnm.",
+   "zxcvbnm1",
+   "zxcvbnm12",
+   "zxcvbnm123",
+   "zxcvbnm123456789",
+   "zxcvbnma",
+   "zxcvbnmm",
+   "zxcvbnmz",
+   "zxcvfdsa",
+   "zxcvvcxz",
+   "zz123456",
+   "zz8807zpl",
+   "zzxxccvv",
+   "zzzxxxccc",
+   "zzzzxxxx",
+   "zzzzzzz1",
+];
+
+var commonTest = passwordList.filter((word) => word.length > 8 === true);
+console.log(commonTest);
+
 //  Email and Password Validation
 $("#lets-go-button").click(function () {
    // emailInput is the value of the user input for email address
@@ -68,6 +484,8 @@ $("#lets-go-button").click(function () {
       $("#passwordErrorMessage").text(
          "Your email address cannot be used in your password."
       );
+   } else if (passwordInput.includes(commonTest) != true) {
+      $("#passwordErrorMessage").text("Please enter a less common password");
    } else {
       validPassword = true;
    }
@@ -85,25 +503,25 @@ $("#lets-go-button").click(function () {
       $("#new-userPassword").removeClass("is-valid");
    }
 
-   // Generate _id and created On Date
-
+   // New User Submission Entry - once email and password are valid let's go button
+   // will generate a new id in the console and also noted the exact date and time of entry
    // calling my createTheDate function to generate the createdOn Date
-   // this function includes the timeMilliseconds variable for generate _id
    createTheDate();
-
    //  variable for random number for generate _id
    var randomNumberForId = Math.floor(Math.random() * 1000);
    console.log(randomNumberForId);
-
-   // concatenate timeMilliseconds and randomNumberFor Id to create new id when email and password are valid
-   var getNewId = "" + timeMilliseconds + randomNumberForId;
+   // concatenate timeMilliseconds & randomNumberForId to create new six-digit id
+   var getNewId =
+      "" +
+      addThreePadding(timeMilliseconds) +
+      addThreePadding(randomNumberForId);
 
    if (userEmailValid && validPassword) {
       var newUserSubmission = {
          _id: getNewId,
          email: emailInput,
          password: passwordInput,
-         createdOn: getToday,
+         createdOn: Number(getToday),
       };
    }
    console.log(newUserSubmission);
@@ -137,17 +555,18 @@ $("#answerArea").keyup(function () {
 $("#save-imageryButton").click(function () {
    $("#overlay-success").toggleClass("d-none d-flex");
 
-   // // Date variables
-
-   createTheDate();
-
    //  variable for random number for Id
    var randomNumberForId = Math.floor(Math.random() * 1000);
    console.log(randomNumberForId);
+   // concatenate timeMilliseconds & randomNumberForId to create new six-digit id
+   var getNewId =
+      "" +
+      addThreePadding(timeMilliseconds) +
+      addThreePadding(randomNumberForId);
 
-   // concatenate timeMilliseconds and randomNumberFor Id to create new id when email and password are valid
-   var getNewId = "" + timeMilliseconds + randomNumberForId;
-
+   // calling the date for createdOn and the timeMilliseconds for id
+   createTheDate();
+   // userImageryInput tracks user input
    var userImageryInput = {
       _id: getNewId,
       imagery:
@@ -172,43 +591,3 @@ $("#customCheck").click(function () {
 $("#dropdown-toggle-all").click(function () {
    $("#dropdown-menu-list").toggleClass("show");
 });
-
-// Create the date function
-var getToday = "";
-var timeMilliseconds = "";
-
-function createTheDate() {
-   var getTodayDate = new Date();
-   var dateYear = getTodayDate.getYear() - 100;
-   console.log(dateYear);
-   var dateMonth = getTodayDate.getMonth() + 1;
-   console.log(dateMonth);
-   var dateDay = getTodayDate.getDate();
-   console.log(dateDay);
-   var timeHours = getTodayDate.getHours();
-   console.log(timeHours);
-   var timeMinutes = getTodayDate.getMinutes();
-   console.log(timeMinutes);
-   var timeSeconds = getTodayDate.getSeconds();
-   console.log(timeSeconds);
-   timeMilliseconds = getTodayDate.getMilliseconds();
-   console.log(timeMilliseconds);
-
-   getToday =
-      "" + //turns it into a readable string
-      addPadding(dateYear) +
-      addPadding(dateMonth) +
-      addPadding(dateDay) +
-      addPadding(timeHours) +
-      addPadding(timeMinutes) +
-      addPadding(timeSeconds);
-   console.log(getToday);
-}
-
-function addPadding(num) {
-   if (String(num).length < 2) {
-      return "0" + num.toString();
-   } else {
-      return num;
-   }
-}
